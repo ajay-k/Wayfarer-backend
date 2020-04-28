ad89fdf455b082993799b10fa233334898957ab4
ajay-k <ajay_k@sbcglobal.net>
2020-04-27 21:18:09 -0700

    Fixes backend

--- controllers/city.js
+++ controllers/city.js
@@ -20,58 +20,58 @@ const cityShow = (req, res) => {
     })
 }
 
-const addPost = (req, res) => {
-    db.Post.create(req.body ,(err, newPost) => {
-        if (err) {
-            return res.status(400).json({ status: 400, error: 'Something went wrong' });
-        }
-    db.City.findById(req.params.id, (err, foundCity) => {
-        if (err) {
-            return res.status(400).json({ status: 400, error: 'Something went wrond' })
-        }
-        foundCity.posts.push(newPost);
-        foundCity.save((err, savedPost) => {
-            if (err) {
-                return res.status(400).json({ status: 400, error: 'Something is wrong' });
-            }
-            res.json(savedPost)
-        })
-    })
-    })
-}
-
-
-// const newAddPost = (req, res) => {
-//     db.Post.create(req.body, (err, newPost) => {
+// const addPost = (req, res) => {
+//     db.Post.create(req.body ,(err, newPost) => {
 //         if (err) {
 //             return res.status(400).json({ status: 400, error: 'Something went wrong' });
 //         }
-//         db.City.findById(req.params.cityId, (err, foundCity) => {
+//     db.City.findById(req.params.id, (err, foundCity) => {
+//         if (err) {
+//             return res.status(400).json({ status: 400, error: 'Something went wrond' })
+//         }
+//         foundCity.posts.push(newPost);
+//         foundCity.save((err, savedPost) => {
 //             if (err) {
-//                 return res.status(400).json({ status: 400, error: 'Something went wrond' })
+//                 return res.status(400).json({ status: 400, error: 'Something is wrong' });
 //             }
-//             foundCity.posts.push(newPost);
-//             foundCity.save((err, savedPost) => {
-//                 if (err) {
-//                     return res.status(400).json({ status: 400, error: 'Something is wrong' });
-//                 }
-//                 res.json(savedPost)
-//             })
-//         })
-//         db.User.findById(req.params.userId, (err, foundUser) => {
-//             foundUser.posts.push(newPost);
-//             foundUser.save((err, savedPost) => {
-//                 if (err) {
-//                     return res.status(400).json({ status: 400, error: 'Something is wrong' });
-//                 }
-//                 res.json(savedPost)
-//             })
+//             res.json(savedPost)
 //         })
 //     })
+//     })
 // }
+
+
+const newAddPost = (req, res) => {
+    db.Post.create(req.body, (err, newPost) => {
+        if (err) {
+            return res.status(400).json({ status: 400, error: 'Something went wrong' });
+        }
+        db.City.findById(req.params.cityId, (err, foundCity) => {
+            if (err) {
+                return res.status(400).json({ status: 400, error: 'Something went wrond' })
+            }
+            foundCity.posts.push(newPost);
+            foundCity.save((err, savedPost) => {
+                if (err) {
+                    return res.status(400).json({ status: 400, error: 'Something is wrong' });
+                }
+                res.json(savedPost)
+            })
+        })
+        db.User.findById(req.params.userId, (err, foundUser) => {
+            foundUser.posts.push(newPost);
+            foundUser.save((err, savedPost) => {
+                if (err) {
+                    return res.status(400).json({ status: 400, error: 'Something is wrong' });
+                }
+                res.json(savedPost)
+            })
+        })
+    })
+}
 
 module.exports = {
     view,
     cityShow,
-    addPost,
+    newAddPost,
 }
